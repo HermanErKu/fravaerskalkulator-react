4e9625a685d2d2d32662afbb06c8a2f1b1e4c2b7
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-05-27 13:54:37 +0200

    added features for VG2 to use

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
@@ -14,38 +14,10 @@ function App() {
   //engelsk                             6
 
   const fag_data = {
-    0:{
-      name: "Produksjon og historiefortelling",
-      ukeTimer: 5.5
-    },
-    1:{
-      name: "Konseptutvikling og programmering",
-      ukeTimer: 5.5
-    },
-    2:{
-      name: "Teknologiforståelse",
-      ukeTimer: 5.5
-    },
-    3:{
-      name: "YFF",
-      ukeTimer: 6
-    },
-    4:{
-      name: "Engelsk",
-      ukeTimer: 5
-    },
-    5:{
-      name: "Matte",
-      ukeTimer: 3
-    },
-    6:{
-      name: "Naturfag",
-      ukeTimer: 2
-    },
-    7:{
-      name: "Gym",
-      ukeTimer: 2
-    },
+    "null": {},
+    0: { 0: { name: "Produksjon og historiefortelling", ukeTimer: 5.5 }, 1: { name: "Konseptutvikling og programmering", ukeTimer: 5.5 }, 2: { name: "Teknologiforståelse", ukeTimer: 5.5 }, 3: { name: "YFF", ukeTimer: 6 }, 4: { name: "Engelsk", ukeTimer: 5 }, 5: { name: "Matte", ukeTimer: 3 }, 6: { name: "Naturfag", ukeTimer: 2 }, 7: { name: "Gym", ukeTimer: 2 }, name:"VG1", fag:8},
+    1: { 0: { name: "Brukerstøtte", ukeTimer: 5 }, 1: { name: "Driftsstøtte", ukeTimer: 6 }, 2: { name: "Utvikling", ukeTimer: 6 }, 3: { name: "YFF", ukeTimer: 9 }, 4: { name: "Norsk", ukeTimer: 4 }, 5: { name: "Samfunnsfag", ukeTimer: 3 }, 6: { name: "Gym", ukeTimer: 2 }, name:"VG2 - IT", fag:7},
+    2: { 0: { name: "Design og visualisering", ukeTimer: 5.5 }, 1: { name: "Konseptutvikling og kommunikasjon", ukeTimer: 5.5 }, 2: { name: "Teknologi og produksjon", ukeTimer: 5.5 }, 3: { name: "YFF", ukeTimer: 9 }, 4: { name: "Norsk", ukeTimer: 4 }, 5: { name: "Samfunnsfag", ukeTimer: 3 }, 6: { name: "Gym", ukeTimer: 2 }, name:"VG2 - Medie", fag:7},
   }
 
   const [fagID, setFagID] = useState(0);
@@ -61,9 +33,33 @@ function App() {
   const [wholePercent, setWholePercent] = useState(0);
   const [halfPercent, setHalfPercent] = useState(0);
   const calculatePercent = () => {
-    setWholePercent((timerVekke / (parseInt(fag_data[fagID].ukeTimer) * 38) * 100).toFixed(2))
-    setHalfPercent((timerVekke / (parseInt(fag_data[fagID].ukeTimer) * 19) * 100).toFixed(2))
+    setWholePercent((timerVekke / (parseInt(fag_data[yearID][fagID].ukeTimer) * 38) * 100).toFixed(2))
+    setHalfPercent((timerVekke / (parseInt(fag_data[yearID][fagID].ukeTimer) * 19) * 100).toFixed(2))
   }
+
+
+  const [yearID, setYearID] = useState(0);
+  const [optionsArray, setOptionsArray] = useState([]);
+  const handleYearChange = (event) => {
+    const year = event.target.value;
+    setYearID(year);
+    let options = [];
+    for (let x = 0; x < fag_data[year].fag; x++) {
+      options.push(<option key={x} value={x}>{fag_data[year][x].name}</option>);
+    }
+    setOptionsArray(options);
+
+    setFagID(0);
+  }
+
+  /*const [optionsElement, setOptionsElement] = useState("");
+  console.log(fag_data[fagID].fag);
+  for (let x = 0; x < fag_data[fagID].fag; x++) {
+    //const element = fag_data[fagID][x];
+    setOptionsElement("<option value='7'>{fag_data[yearID][7].name}</option>")
+  }*/
+
+  
 
 
 
@@ -72,23 +68,26 @@ function App() {
       <h1>Informasjonsteknologi og medieproduksjon:</h1>
       
       <form>
-        <select name="year" id="year">
-          <option value="year_1">VG1</option>
-          <option value="year_2" disabled="disabled">VG2</option>
+        <select name="year" id="year" onChange={handleYearChange}>
+          <option value="null">Velg ett årsløp...</option>
+          <option value="0">VG1</option>
+          <option value="1">VG2 - IT</option>
+          <option value="2">VG2 - Medie</option>
         </select>
 
         <h3>Hvor mange timer skal du ta fri?</h3>
 
         <label htmlFor="fag">Hvilket fag skal du ta fri?</label><br/>
         <select name="fag" id="fag" onChange={handleFagChange}>
-          <option value="0">{fag_data[0].name}</option>
-          <option value="1">{fag_data[1].name}</option>
-          <option value="2">{fag_data[2].name}</option>
-          <option value="3">{fag_data[3].name}</option>
-          <option value="4">{fag_data[4].name}</option>
-          <option value="5">{fag_data[5].name}</option>
-          <option value="6">{fag_data[6].name}</option>
-          <option value="7">{fag_data[7].name}</option>
+          {optionsArray}
+          {/*<option value="0">{fag_data[yearID][0].name}</option>
+          <option value="1">{fag_data[yearID][1].name}</option>
+          <option value="2">{fag_data[yearID][2].name}</option>
+          <option value="3">{fag_data[yearID][3].name}</option>
+          <option value="4">{fag_data[yearID][4].name}</option>
+          <option value="5">{fag_data[yearID][5].name}</option>
+          <option value="6">{fag_data[yearID][6].name}</option>
+          <option value="7">{fag_data[yearID][7].name}</option>*/}
         </select><br /><br />
 
         <label htmlFor="timer">Hvor mange timer skal du ta fri?</label><br />
