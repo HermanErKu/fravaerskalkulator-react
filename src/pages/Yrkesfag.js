d5e56365770f745baee8e3a9364a186cb0afc045
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-05-29 15:41:12 +0200

    added functioning site by using old app component

--- src/pages/Yrkesfag.js
+++ src/pages/Yrkesfag.js
@@ -1,7 +1,13 @@
-
+import App from '../App';
 
 const Yrkesfag = () => {
-    return <h1>Yrkesfag</h1>;
+    return (
+        <div>
+            <h2 style={{textAlign:'center'}}>⚠️ Siden er under oppussing! ⚠️</h2>
+            <App/>
+            <h2 style={{textAlign:'center'}}>⚠️ Siden er under oppussing! ⚠️</h2>
+        </div>
+    );
   };
   
   export default Yrkesfag;
