06816a5c02a0294633d94181f994912d180c00f9
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-05-29 15:26:31 +0200

    added new imports for react router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Index from './pages/Index';
+import Studiefag from './pages/Studiefag';
+import Yrkesfag from './pages/Yrkesfag';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
