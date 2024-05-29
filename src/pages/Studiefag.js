5f789045c4432a73083caa9d476cc2a85476692a
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-05-29 15:34:50 +0200

    made all pages render functional

---
+++ src/pages/Studiefag.js
@@ -0,0 +1,7 @@
+
+
+const Studiefag = () => {
+    return <h1>Studiefag</h1>;
+  };
+  
+  export default Studiefag;
---
+++ src/pages/Yrkesfag.js
@@ -0,0 +1,7 @@
+
+
+const Yrkesfag = () => {
+    return <h1>Yrkesfag</h1>;
+  };
+  
+  export default Yrkesfag;
