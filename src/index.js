44051be322b4ba4b92a6d0dd4c7685637348b42d
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-05-29 15:35:09 +0200

    fixed react router

--- src/index.js
+++ src/index.js
@@ -29,21 +29,17 @@ reportWebVitals();
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+import Index from './pages/Index';
+import Studiefag from './pages/Studiefag';
+import Yrkesfag from './pages/Yrkesfag';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
+        <Route index element={<Index />} />
+        <Route path="studiefag" element={<Studiefag />} />
+        <Route path="yrkesfag" element={<Yrkesfag />} />
       </Routes>
     </BrowserRouter>
   );
