46e42fe7735c39a1ae688b9ec1a45df38a069db5
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-05-29 22:30:09 +0200

    added support for all yrkesfag and loading them in their own page using url parameters

--- src/pages/Yrkesfag.js
+++ src/pages/Yrkesfag.js
@@ -1,13 +1,92 @@
 import App from '../App';
+import { useSearchParams } from "react-router-dom"
+
+
+import Bygg from '../pages/yrkesfag/Bygg';
+import Elektro from '../pages/yrkesfag/Elektro';
+import Frisør from '../pages/yrkesfag/Frisør';
+import Helse from '../pages/yrkesfag/Helse';
+import Håndverk from '../pages/yrkesfag/Håndverk';
+import IM from '../pages/yrkesfag/IM';
+import Naturbruk from '../pages/yrkesfag/Naturbruk';
+import Restaurant from '../pages/yrkesfag/Restaurant';
+import Salg from '../pages/yrkesfag/Salg';
+import TIF from '../pages/yrkesfag/TIF';
+
 
 const Yrkesfag = () => {
+  const [queryParameters] = useSearchParams()
+  const linjeId = queryParameters.get("linjeId")
+  console.log(linjeId)
+
+  if (linjeId === null) {
     return (
-        <div>
-            <h2 style={{textAlign:'center'}}>⚠️ Siden er under oppussing! ⚠️</h2>
-            <App/>
-            <h2 style={{textAlign:'center'}}>⚠️ Siden er under oppussing! ⚠️</h2>
+      <div>
+        <h2 style={{ textAlign: 'center' }}>⚠️ Siden er under oppussing! ⚠️</h2>
+        {/*<App/>*/}
+        <div id="linjeContainer">
+          <a href="/yrkesfag?linjeId=0"> <div className="linje" id="bygg"> <h2>Bygg- og anleggsteknikk</h2> </div> </a>
+          <a href="/yrkesfag?linjeId=1"> <div className="linje" id="elektro"> <h2>Elektro og datateknologi</h2> </div> </a>
+          <a href="/yrkesfag?linjeId=2"> <div className="linje" id="frisør"> <h2>Frisør, blomster, interiør og eksponeringsdesign</h2> </div> </a>
+          <a href="/yrkesfag?linjeId=3"> <div className="linje" id="helse"> <h2>Helse- og oppvekstfag</h2> </div> </a>
+          <a href="/yrkesfag?linjeId=4"> <div className="linje" id="håndverk"> <h2>Håndverk, design og produktutvikling</h2> </div> </a> 
+          <a href="/yrkesfag?linjeId=5"> <div className="linje" id="im"> <h2>Informasjonsteknologi og medieproduksjon</h2> </div> </a>
+          <a href="/yrkesfag?linjeId=6"> <div className="linje" id="naturbruk"> <h2>Naturbruk</h2> </div> </a> 
+          <a href="/yrkesfag?linjeId=7"> <div className="linje" id="restaurant"> <h2>Restaurant- og matfag</h2> </div> </a> 
+          <a href="/yrkesfag?linjeId=8"> <div className="linje" id="salg"> <h2>Salg, service og reiseliv</h2> </div> </a> 
+          <a href="/yrkesfag?linjeId=9"> <div className="linje" id="tif"> <h2>Teknologi- og industrifag</h2> </div> </a>
         </div>
+        <h2 style={{ textAlign: 'center' }}>⚠️ Siden er under oppussing! ⚠️</h2>
+      </div>
     );
-  };
+  } else if (linjeId === "0") {
+    return (
+      <Bygg />
+    );
+  } else if (linjeId === "1") {
+    return (
+      <Elektro />
+    );
+  } else if (linjeId === "2") {
+    return (
+      <Frisør />
+    );
+  } else if (linjeId === "3") {
+    return (
+      <Helse />
+    );
+  } else if (linjeId === "4") {
+    return (
+      <Håndverk />
+    );
+  } else if (linjeId === "5") {
+    return (
+      <IM />
+    );
+  } else if (linjeId === "6") {
+    return (
+      <Naturbruk />
+    );
+  } else if (linjeId === "7") {
+    return (
+      <Restaurant />
+    );
+  } else if (linjeId === "8") {
+    return (
+      <Salg />
+    );
+  } else if (linjeId === "9") {
+    return (
+      <TIF />
+    );
+
+  } else {
+    return (
+      <h1>siden finnes ikke</h1>
+    );
+  }
+
   
-  export default Yrkesfag;
+};
+
+export default Yrkesfag;
