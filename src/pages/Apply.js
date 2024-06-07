8e6495b0bcd9b31186e8e98da81681611cf2b66e
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-06-07 13:14:14 +0200

    fixed styling of apply page

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -1,12 +1,7 @@
-import './Apply.css'
-
-
 const Apply = () => {
-
-
     return (
-        <div style={{margin: "0px"}}>
-            <iframe width="640px" height="480px" src="https://forms.office.com/e/aeQBmHmNr1?embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border:"none", width:"100vw", height:"100vh"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+        <div>
+            <iframe width="640px" height="480px" src="https://forms.office.com/e/aeQBmHmNr1?embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border:"none", width:"99vw", height:"97.3vh", maxWidth:"100%", maxHeight:"100%"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
         </div>
     );
 }
