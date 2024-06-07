b6d3d4ab729d362ff7b72a4a645fdaeb167745e0
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-06-07 13:01:37 +0200

    made finished apply page

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -1,11 +1,12 @@
-
-
+import './Apply.css'
 
 
 const Apply = () => {
+
+
     return (
-        <div>
-            <h1>Apply</h1>
+        <div style={{margin: "0px"}}>
+            <iframe width="640px" height="480px" src="https://forms.office.com/e/aeQBmHmNr1?embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border:"none", width:"100vw", height:"100vh"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
         </div>
     );
 }
