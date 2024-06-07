fc576065f78e457d149e3d75e93ea78f02f83ade
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-06-07 21:30:44 +0200

    started adding functionality to studiefag

--- src/pages/Studiefag.js
+++ src/pages/Studiefag.js
@@ -1,14 +1,71 @@
 import { useSearchParams } from "react-router-dom";
+import Cookies from 'universal-cookie';
+import Calculator from './Calculator';
+import '../../src/App.css';
 
 
 const Studiefag = () => {
   const [queryParameters] = useSearchParams()
   const linjeId = queryParameters.get("linjeId")
-  console.log(linjeId)
+
+
+  const cookies = new Cookies();
+  const setMinLinje = () => {
+    cookies.set('linjeCookie', '/yrkesfag?linjeId='+linjeId, { path: '/' });
+  }
+
+  const studiefag_data = {
+    0: { 
+      name:"Idrettsfag",
+      year_data: {
+        0: { name:"Velg ett årsløp..." },
+        1: {}
+      }
+    },
+    1: { 
+      name:"Kunst, design og arkitektur",
+      year_data: {
+        0: { name:"Velg ett årsløp..." },
+        1: {}
+      }
+    },
+    2: { 
+      name:"Medier og kommunikasjon",
+      year_data: {
+        0: { name:"Velg ett årsløp..." },
+        1: {}
+      }
+    },
+    3: { 
+      name:"Musikk, dans og drama",
+      year_data: {
+        0: { name:"Velg ett årsløp..." },
+        1: {}
+      }
+    },
+    4: { 
+      name:"Studiespesialisering",
+      year_data: {
+        0: { name:"Velg ett årsløp..." },
+        1: {}
+      }
+    },
+  }
+
+
+  const fag_data = studiefag_data[parseInt(linjeId)]["year_data"]
 
   return (
     <div>
-      <h1>Studiefag</h1>
+      <div className='breadCrumbsContainer'>
+        <a id='breadcrumbsLink' href='/home' color='inherit'>Hjem</a>
+        /
+        <a id='breadcrumbsLink' href={'/studiefag/'+linjeId+'?linjeId='+linjeId} color='text.primary'>{studiefag_data[parseInt(linjeId)]["name"]}</a>
+      </div>
+
+      <Calculator
+        fag_data={fag_data}
+      />
     </div>
   );
 };
