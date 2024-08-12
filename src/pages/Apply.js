193d291a17ff8de6888ee2a45d81e741fd0d30cd
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-08-12 23:20:11 +0200

    fixed all pages containing form with form layout change

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -1,9 +1,20 @@
+import { useNavigate} from "react-router-dom";
+
+
 const Apply = () => {
-    return (
-        <div>
-            <iframe width="640px" height="480px" src="https://forms.office.com/e/aeQBmHmNr1?embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{border:"none", width:"99vw", height:"97.3vh", maxWidth:"100%", maxHeight:"100%"}} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
-        </div>
-    );
+  let navigate = useNavigate();
+
+  return (
+    <div style={{ position:"relative" }}>
+      <div className="goBackButtonForm" style={{ position:"absolute", zIndex:9 }}>
+        <button onClick={() => navigate(-1)}>Gå tilbake</button> 
+      </div>
+
+      <div className="iframeContainerForm" style={{ position:"absolute" }}>
+        <iframe width="640px" height="480px" src="https://forms.office.com/e/aeQBmHmNr1?embed=true" frameborder="0" marginwidth="0" marginheight="0" style={{ border: "none", width: "99vw", height: "97.3vh", maxWidth: "100%", maxHeight: "100%" }} allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+      </div>
+    </div>
+  )
 }
 
 export default Apply;
